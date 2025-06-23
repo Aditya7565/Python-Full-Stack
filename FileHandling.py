2303052e31a7a5cc7d37f9c8c8620c5f24f793dc
Aditya Maurya <mauryaaditya7565@gmail.com>
2025-06-23 09:46:09 +0530

    f

---
+++ FileHandling.py
@@ -0,0 +1,38 @@
+
+# f=open("test.txt")
+# read_txt=f.read()
+# print(read_txt)
+# f.close()
+
+# f=open("test.txt")
+# read_txt=f.readline()
+# print(read_txt)
+# f.close()
+
+
+# f=open("test.txt")
+# read_txt=f.readlines()
+# print(read_txt)
+# f.close()
+
+
+# f=open("C:\Users\HP\OneDrive\Desktop\file1.txt","r") it is wrong
+# f=open("C:/Users/HP/OneDrive/Desktop/file1.txt","r")
+
+# read_txt=f.read()
+# print(read_txt,"\n")
+# f.close()
+
+
+
+# f=open("test.txt")
+# write_txt=
+
+
+
+# f=open("test.txt")
+# read_txt=f.readlines()
+# for i in read_txt:
+#     print(i)
+# print(read_txt)
+# f.close()
